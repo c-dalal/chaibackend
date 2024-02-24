5491deef833d60b972dcca29933c992f3f387586
chandni dalal <chandnidalal2@gmail.com>
2024-02-24 22:02:29 +0530

    Utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constant";
-// import express from "express";
+import express from "express";
 // require('dotenv').config();
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
@@ -9,7 +9,21 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+const app = express();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+
+    app.on("error", () =>{
+                    console.log("Err: ",error);
+                    throw error
+                })
+})
+.catch((err) => {
+    console.log("Mongo db Connection Failed",err);
+})
 
 
 // const app = express();
